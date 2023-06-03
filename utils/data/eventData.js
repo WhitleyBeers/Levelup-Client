6d3f999a42cd6a9701367bf746bc4d57fc6cbfaf
Whitley Beers <whitleybeers@gmail.com>
2023-06-03 11:51:33 -0500

    join and leave events working and buttons on cards

--- utils/data/eventData.js
+++ utils/data/eventData.js
@@ -1,14 +1,25 @@
 import { clientCredentials } from '../client';
 
-const getEvents = () => new Promise((resolve, reject) => {
-  fetch(`${clientCredentials.databaseURL}/events`)
+const getEvents = (uid) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/events`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `${uid}`,
+    },
+  })
     .then((response) => response.json())
     .then(resolve)
     .catch(reject);
 });
 
 const getSingleEvent = (id) => new Promise((resolve, reject) => {
-  fetch(`${clientCredentials.databaseURL}/events/${id}`)
+  fetch(`${clientCredentials.databaseURL}/events/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
     .then((response) => response.json())
     .then(resolve)
     .catch(reject);
@@ -51,6 +62,30 @@ const deleteEvent = (id) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+const leaveEvent = (eventId, uid) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/events/${eventId}/leave`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `${uid}`,
+    },
+  })
+    .then(resolve)
+    .catch(reject);
+});
+
+const joinEvent = (eventId, uid) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/events/${eventId}/signup`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `${uid}`,
+    },
+  })
+    .then((response) => resolve(response.json()))
+    .catch(reject);
+});
+
 export {
-  getEvents, createEvent, getSingleEvent, updateEvent, deleteEvent,
+  getEvents, createEvent, getSingleEvent, updateEvent, deleteEvent, leaveEvent, joinEvent,
 };
